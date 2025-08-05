6ae2287a808049898da163ad0de082d45c442db2
Yashvardhan Singh <chanchalyashsingh2005@gmail.com>
2025-08-05 20:26:13 +0530

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,12 @@
+def caesar_cipher(text, shift, mode='decode'):
+    if mode == 'decode':
+        shift = -shift
+
+    result = ""
+    for char in text:
+        if char.isalpha():
+            start = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - start + shift) % 26 + start)
+        else:
+            result += char
+    return result
